9b2af822759c2a191afc6bec45eee430ab67f7c2
LuongHung10 <luongviethung9@gmail.com>
2024-03-13 17:32:37 +0700

    fix

--- home/models.py
+++ home/models.py
@@ -60,3 +60,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 300 or img.width > 300:
+            new_img = (300, 300)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
